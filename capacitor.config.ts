5d4a4728686d0bf027c6f13af6433c74d1953428
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2024-10-26 16:59:11 +0200

    copy web assets to native folder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ellinorlindstrom.stridequest',
   appName: 'Stride Quest',
-  webDir: 'dist'
+  webDir: 'build'
 };
 
 export default config;
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
 			strict: true
 		})
